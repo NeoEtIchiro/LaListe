a733bb5828c342b0c84324ac5b33993ea5577d6c
NeoEtIchiro <neoetichiro@gmail.com>
2025-01-28 11:51:18 +0100

    Les mois sont séparé dans la liste des payments

--- src/components/Cash/ShowCash.vue
+++ src/components/Cash/ShowCash.vue
@@ -5,18 +5,25 @@
         Trésorerie
       </h1>
     </div>
-    <div class="flex w-full">
+    <div class="flex w-full h-full">
         <div class="chartDiv w-3/5">
             <PaymentChart class="chartDiv" :payments="payments" />
         </div>
         <div class="w-2/5">
             <button @click="popupVisible = true" class="bg-blue-500 text-white px-4 py-2 rounded">Ajouter un paiement</button>
             <div class="mt-6">
-                <h2 class="text-xl mb-4">Liste des paiements</h2>
-                <div class="flex justify-center w-full mb-1" v-for="payment in payments" :key="payment.id" @dblclick="selectedPayment = payment; popupVisible = true">
-                    <div class="basicDiv mr-1 w-full whitespace-nowrap">{{ payment.name }}</div>
-                    <div class="basicDiv mr-1 whitespace-nowrap">{{ payment.date }}</div>
-                    <div class="basicDiv">{{ payment.amount }}€</div>
+                <div class="overflow-y-auto max-h-96 border border-gray-300 rounded-lg p-4">
+                    <template v-for="(payment, index) in payments" :key="payment.id">
+                        <div v-if="index === 0 || new Date(payment.date).getMonth() !== new Date(payments[index - 1].date).getMonth()" class="w-full flex items-center">
+                            <span class="mr-2 mb-1 mt-1">{{ getMonthName(new Date(payment.date).getMonth()) }}</span>
+                            <hr class="flex-grow border-gray-300">
+                        </div>
+                        <div class="flex justify-center w-full mb-1" @dblclick="selectedPayment = payment; popupVisible = true">
+                            <div class="basicDiv mr-1 w-full whitespace-nowrap">{{ payment.name }}</div>
+                            <div class="basicDiv mr-1 whitespace-nowrap">{{ payment.date }}</div>
+                            <div class="basicDiv">{{ payment.amount }}€</div>
+                        </div>
+                    </template>
                 </div>
             </div>
         </div>
@@ -60,6 +67,13 @@ export default defineComponent({
     };
   },
   methods: {
+    getMonthName(monthIndex) {
+      const monthNames = [
+        'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+        'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
+      ];
+      return monthNames[monthIndex];
+    },
     async addNewPayment(payment) {
         switch (payment.frequency) {
             case 'unique':
@@ -107,5 +121,14 @@ export default defineComponent({
 <style scoped>
 .chartDiv{
     height: 400px;
+}
+.basicDiv {
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+}
+
+.listPayments{
+  border: 1px black;
 }
 </style>
